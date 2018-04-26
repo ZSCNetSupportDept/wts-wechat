c84da35033b1531600559a8941320f95a07ec823
Sola <dev@sola.love>
2018-04-26 10:27:55 +0800

    init
    Signed-off-by: Sola <dev@sola.love>

---
+++ src/main/kotlin/love/sola/zscnsd/wts/wechat/WtsWechatApplication.kt
@@ -0,0 +1,11 @@
+package love.sola.zscnsd.wts.wechat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WtsWechatApplication
+
+fun main(args: Array<String>) {
+    runApplication<WtsWechatApplication>(*args)
+}
---
+++ src/test/kotlin/love/sola/zscnsd/wts/wechat/WtsWechatApplicationTests.kt
@@ -0,0 +1,16 @@
+package love.sola.zscnsd.wts.wechat
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class WtsWechatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
